09eea22f43eee2487db9e0b977027413578e48ea
ivanrhcp20 <ivanm-86@hotmail.com>
2015-06-30 16:35:25 -0300

    ABM de clientes y proveedores funcionando

---
+++ application/models/abm_model.php
@@ -0,0 +1,52 @@
+<?php 
+class Abm_model extends CI_Model {
+ function __construct()
+    {
+        parent::__construct();
+    }
+    function listar($tabla)
+    { 
+       $sql = $this->db->get($tabla);
+        return $sql->result();
+    }
+    function listar_personas($tipo, $tabla)
+    {
+      $this->db->where('tipo_persona',$tipo);
+      $sql = $this->db->get($tabla);
+      return $sql->result();
+    }
+   function agregar($tabla, $data){
+    $this->db->insert($tabla,$data);
+       
+    }
+   function modificar($id,$campoid,$tabla,$data){
+      $this->db->where($campoid,$id);
+      $sql= $this->db->update($tabla,$data);
+      
+    }
+    function eliminar($id,$campoid,$tabla){
+       $this->db->where($campoid,$id);
+      $sql= $this->db->delete($tabla);
+      
+    }
+    function utimos_10_registro($tabla)
+    {
+        $query = $this->db->get($tabla, 10);
+        return $query->result();
+    }
+    function registro_especifico($id,$campoid,$tabla)
+    {
+      $this->db->where($campoid,$id);
+      $registro = $this->db->get($tabla);
+
+      $resultado = $registro -> result();
+      return $resultado;
+    }
+    function buscarpersona($tabla,$palabra,$tipo_persona){
+      $this->db->where('tipo_persona',$tipo_persona);
+      $this->db->like('razon_social',$palabra);
+      $sql = $this->db->get('personas');
+      return $sql -> result();
+    }
+} 
+ ?>
